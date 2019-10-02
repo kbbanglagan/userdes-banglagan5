d66b1804b7ce7d6e23eeae2d7e1f27418c8f80f0
kbbanglagan <52182417+kbbanglagan@users.noreply.github.com>
2019-10-02 12:45:09 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,184 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>labproject</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/unicons.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <!-- MAIN STYLE -->
+    <link rel="stylesheet" href="css/tooplate-style.css">
+    
+<!--
+
+Tooplate 2115 Marvel
+
+https://www.tooplate.com/view/2115-marvel
+
+-->
+  </head>
+  <body>
+
+    <!-- MENU -->
+    <nav class="navbar navbar-expand-sm navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="index.html"><i class='uil uil-user'></i>Home</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item">
+                        <a href="#about" class="nav-link"><span data-hover="About">About</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#resume" class="nav-link"><span data-hover="Learnings">Learnings</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="activities.html" class="nav-link"><span data-hover="Activities">Activities</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="reflection.html" class="nav-link"><span data-hover="Reflections">Reflections</span></a>
+                    </li>
+               </ul>
+                <ul class="navbar-nav ml-lg-auto">
+                    <div class="ml-lg-4">
+                      <div class="color-mode d-lg-flex justify-content-center align-items-center">
+                        <i class="color-mode-icon"></i>
+                        Day/Night mode
+                      </div>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- ABOUT -->
+    <section class="about full-screen d-lg-flex justify-content-center align-items-center" id="about">
+        <div class="container">
+            <div class="row">
+                
+                <div class="col-lg-7 col-md-12 col-12 d-flex align-items-center">
+                    <div class="about-text">
+                        <small class="small-text">Welcome to <span class="mobile-block">my portfolio website!</span></small>
+                        <h1 class="animated animated-text">
+                            <span class="mr-2">Hey guys! I'm </span>
+                                <div class="animated-info">
+                                    <span class="animated-item">Kevin</span>
+                                    <span class="animated-item">an I.T Student</span>
+                                    <span class="animated-item">a Gamer</span>
+                                </div>
+                        </h1>
+
+                        <p>From BSIT-MI181. 18 years old. I like putting together pc parts, cleaning computer and help friends who are in need creating their own computer setup.</p>
+                        
+                        <div class="custom-btn-group mt-4">
+            
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-5 col-md-12 col-12">
+                    <div class="about-image svg">
+                        <img src="images/undraw/dp.jpg" class="img-fluid" alt="svg image">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
+    <!-- FEATURES -->
+    <section class="resume py-5 d-lg-flex justify-content-center align-items-center" id="resume">
+        <div class="container">
+            <div class="row">
+
+            	<div class="col-lg-12 mx-auto">
+                        <h2>My Linkedin Certificates</h2>
+                      </div>
+
+                <div class="col-lg-6 text-center col-12">
+                    <br></br>
+                    <h2> Java 8 Essential Training </h2>
+                  <img src="images/project/java.JPG" class="img-fluid" alt="">
+
+                </div>
+
+                <div class="col-lg-6 text-center col-12">
+                    <br></br>
+                    <h2> Learning Python </h2>
+                  <img src="images/project/lp.JPG" class="img-fluid" alt="">
+
+                </div>
+
+                <div class="col-lg-6 text-center col-12">
+                    <br></br>
+                    <h2> Microsoft Word 2016 Specialist  </h2>
+                  <img src="images/project/msw.JPG" class="img-fluid" alt="">
+
+                </div>
+
+                <div class="col-lg-6 text-center col-12">
+                    <br></br>
+                    <h2> UX Foundations: Accessibility </h2>
+                  <img src="images/project/uxa.JPG" class="img-fluid" alt="">
+
+                </div>
+
+                <div class="col-lg-6 text-center col-12">
+                    <br></br>
+                    <h2> UX Foundations: Prototyping </h2>
+                  <img src="images/project/uxp.JPG" class="img-fluid" alt="">
+                </div>
+
+                <div class="col-lg-6 text-center col-12">
+                    <br></br>
+                    <h2> UX Foundations: Research </h2>
+                	<img src="images/project/uxr.JPG" class="img-fluid" alt="">
+                    
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+
+    <!-- FOOTER -->
+     <footer class="footer py-5">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-lg-12 col-12">                                
+                        <p class="copyright-text text-center">Copyright &copy; Kevin Banglagan. All rights reserved</p>
+
+                    </div>
+                    
+               </div>
+          </div>
+     </footer>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/Headroom.js"></script>
+    <script src="js/jQuery.headroom.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/smoothscroll.js"></script>
+    <script src="js/custom.js"></script>
+
+  </body>
+</html>
